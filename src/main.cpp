d35a2004b78943ded8201b089b36f35746383c60
armrest99 <amrithkrishnakumar99@gmail.com>
2022-09-26 20:06:00 -0700

    amrith curvy functions

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include <cstdlib>
 #include <string>
 
+#define AUTON 0
+
 const float PI = 3.1415926;
 int flySpeed = 0;
 int inSpeed = 0;
@@ -12,7 +14,7 @@ bool r1Engaged = false;
 bool r2Engaged = false;
 bool l1Engaged = false;
 bool l2Engaged = false;
-
+int angleOffset;
 /**
  * A callback function for LLEMU's center button.
  *
@@ -78,6 +80,32 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
+
+ void stopHold(){
+	driveLeftBack.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+	driveLeftFront.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+	driveRightBack.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+	driveRightFront.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+
+	driveLeftBack.move(0);
+	driveLeftFront.move(0);
+	driveRightBack.move(0);
+	driveRightFront.move(0);
+}
+
+
+void stopCoast(){
+	driveLeftBack.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+	driveLeftFront.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+	driveRightBack.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+	driveRightFront.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+
+	driveLeftBack.move(0);
+	driveLeftFront.move(0);
+	driveRightBack.move(0);
+	driveRightFront.move(0);
+}
+
 void toggleFlySpeed() {
 	if(flySpeed == 0){
 		flySpeed = 1;
@@ -122,82 +150,238 @@ void toggleIntake() {
 		intake = 0;
 	}
 }
+void driveForward(int speed, int distance, int goalHeading){
+	goalHeading = goalHeading-angleOffset;
+	if(goalHeading < 0) goalHeading += 360;
 
-void auton1() {
-  intake.move(127);
-  flywheel.move(127);
-  drive(-30, 30, -315);
-  pros::delay(100);
-  drive(-45, 45, 35);
-  pros::delay(500);
-  indexer.set_value(true);
-  pros::delay(300);
-  indexer.set_value(false);
-  pros::delay(300);
-  indexer.set_value(true);
-  pros::delay(300);
-  indexer.set_value(false);
-  pros::delay(300);
-  drive(-60, 60, 45);
-  indexer.set_value(true);
-  pros::delay(300);
-  indexer.set_value(false);
-  pros::delay(300);
-  indexer.set_value(true);
-  pros::delay(300);
-  indexer.set_value(false);
-  pros::delay(300);
-  drive(-75, 75, 55);
-  indexer.set_value(true);
-  pros::delay(300);
-  indexer.set_value(false);
-  pros::delay(300);
-  indexer.set_value(true);
-  pros::delay(300);
-  indexer.set_value(false);
-  pros::delay(300);
+	bool flag = false;
+
+	int startTime = pros::millis();
+
+	int leftStartDistance = (driveLeftFront.get_position() + driveLeftBack.get_position())/2;
+	int rightStartDistance = (driveRightFront.get_position() + driveRightBack.get_position())/2;
+
+	while(flag == false){
+		double actual_turn;
+		double angle = inertial_sensor.get_heading();
+
+		int leftCurrDistance = (driveLeftFront.get_position() + driveLeftBack.get_position())/2;
+		int rightCurrDistance = (driveRightFront.get_position() + driveRightBack.get_position())/2;
+
+		if((angle < goalHeading && goalHeading-angle < 180) || (angle > goalHeading && angle-goalHeading > 180)){
+			//turnRight
+			actual_turn = 25;
+		}
+		if((angle > goalHeading && angle-goalHeading < 180) || (goalHeading > angle && goalHeading-angle > 180)){
+			//turnLeft
+			actual_turn = -25;
+		}
+
+		pros::lcd::set_text(1, std::to_string(actual_turn));
+
+		driveLeftFront.move(speed + actual_turn);
+		//left_wheel_middle.move(speed + actual_turn);
+		driveLeftBack.move(speed + actual_turn);
+
+		driveRightFront.move(speed - actual_turn);
+		//right_wheel_middle.move(speed - actual_turn);
+		driveRightBack.move(speed - actual_turn);
+
+		if(leftCurrDistance-leftStartDistance > distance && rightCurrDistance-rightStartDistance > distance){
+			flag = true;
+		}
+
+		pros::delay(20);
+	}
+	stopHold();
 }
 
-void auton2() {
-  intake.move(127);
-  flywheel.move(127);
-  drive(90, 1, 0);
-  pros::delay(300);
-  drive(1, 10, -330);
-  indexer.set_value(true);
-  pros::delay(300);
-  indexer.set_value(false);
-  pros::delay(300);
-  indexer.set_value(true);
-  pros::delay(300);
-  indexer.set_value(false);
-  drive(1, 20, -325);
-  indexer.set_value(true);
-  pros::delay(300);
-  indexer.set_value(false);
-  pros::delay(300);
-  indexer.set_value(true);
-  pros::delay(300);
-  indexer.set_value(false);
-  drive(0, 20, -320);
-  indexer.set_value(true);
-  pros::delay(300);
-  indexer.set_value(false);
-  pros::delay(300);
-  indexer.set_value(true);
-  pros::delay(300);
-  indexer.set_value(false);
+void driveReverse(int speed, int distance, int goalHeading){
+	goalHeading = goalHeading-angleOffset;
+	if(goalHeading < 0) goalHeading += 360;
+
+	int startTime = pros::millis();
+
+	bool flag = false;
+
+	int leftStartDistance = (driveLeftFront.get_position() + driveLeftBack.get_position())/2;
+	int rightStartDistance = (driveRightFront.get_position() + driveRightBack.get_position())/2;
+
+	double actual_turn;
+
+	while(flag == false){
+		double angle = inertial_sensor.get_heading();
+
+		int leftCurrDistance = (driveLeftFront.get_position() + driveLeftBack.get_position())/2;
+		int rightCurrDistance = (driveRightFront.get_position() + driveRightBack.get_position())/2;
+
+		if((angle < goalHeading && goalHeading-angle < 180) || (angle > goalHeading && angle-goalHeading > 180)){
+			//turnRight
+			actual_turn = 25;
+		}
+		if((angle > goalHeading && angle-goalHeading < 180) || (goalHeading > angle && goalHeading-angle > 180)){
+			//turnLeft
+			actual_turn = -25;
+		}
+
+		driveLeftFront.move(-speed + actual_turn);
+		//left_wheel_middle.move(-speed + actual_turn);
+		driveLeftBack.move(-speed + actual_turn);
+
+		driveRightFront.move(-speed - actual_turn);
+		//right_wheel_middle.move(-speed - actual_turn);
+		driveRightBack.move(-speed - actual_turn);
+
+		if(leftStartDistance-leftCurrDistance > distance && rightStartDistance-rightCurrDistance > distance){
+			flag = true;
+		}
+
+		pros::delay(20);
+	}
+	stopHold();
 }
 
-void auton3() {
-  flywheel.move(127);
-  rotate(-10);
-  indexer.set_value(true);
-  pros::delay(300);
-  indexer.set_value(false);
+void turn(int speed, int goalHeading, int angleTolerance){
+	goalHeading = goalHeading-angleOffset;
+	if(goalHeading < 0) goalHeading += 360;
+
+	bool flag = false;
+
+	//Angle bounds used to calculate when to stop turning (ex. +-3)
+	int upperAngleBound = goalHeading + angleTolerance;
+	int lowerAngleBound = goalHeading - angleTolerance;
+
+	long begin_time = pros::millis();
+
+	//Special conditions (If the angle is at 0 for example)
+	bool specialDown = false;
+	bool specialUp = false;
+	if(lowerAngleBound < 0){
+		lowerAngleBound = lowerAngleBound + 360;
+		specialDown = true;}
+	if(upperAngleBound > 360){
+		upperAngleBound = upperAngleBound - 360;
+		specialUp = true;}
+
+	while(flag == false){
+		double angle_ = inertial_sensor.get_heading();
+
+		double actual_turn = 10;
+
+		if((angle_ < goalHeading && goalHeading-angle_ < 180) || (angle_ > goalHeading && angle_-goalHeading > 180)){
+			//turnRight
+			actual_turn = speed * (abs(goalHeading-angle_)/100 + 0.3);
+		}
+		if((angle_ > goalHeading && angle_-goalHeading < 180) || (goalHeading > angle_ && goalHeading-angle_ > 180)){
+			//turnLeft
+			actual_turn = -speed * (abs(goalHeading-angle_)/100 + 0.3);
+		}
+
+		driveLeftFront.move(actual_turn);
+		//left_wheel_middle.move(actual_turn);
+		driveLeftBack.move(actual_turn);
+
+		driveRightFront.move(-actual_turn);
+		//right_wheel_middle.move(-actual_turn);
+		driveRightBack.move(-actual_turn);
+
+		if(!((specialDown == true && ((angle_ <  lowerAngleBound && angle_ > goalHeading+180) || (angle_ > upperAngleBound && angle_ < goalHeading+180))) ||
+		(specialUp == true && ((angle_ > upperAngleBound && angle_ < goalHeading-180) || (angle_ < lowerAngleBound && angle_ > goalHeading-180))) ||
+		((specialUp == false && specialDown == false) && (angle_ > upperAngleBound || angle_ < lowerAngleBound)))){
+			flag = true;
+		}
+		pros::delay(20);
+	}
+
+	stopHold();
 }
 
-void autonomous() { auton1(); }
+// void auton1() {
+//   intake.move(127);
+//   flywheel.move(127);
+//   drive(-30, 30, -315);
+//   pros::delay(100);
+//   drive(-45, 45, 35);
+//   pros::delay(500);
+//   indexer.set_value(true);
+//   pros::delay(300);
+//   indexer.set_value(false);
+//   pros::delay(300);
+//   indexer.set_value(true);
+//   pros::delay(300);
+//   indexer.set_value(false);
+//   pros::delay(300);
+//   drive(-60, 60, 45);
+//   indexer.set_value(true);
+//   pros::delay(300);
+//   indexer.set_value(false);
+//   pros::delay(300);
+//   indexer.set_value(true);
+//   pros::delay(300);
+//   indexer.set_value(false);
+//   pros::delay(300);
+//   drive(-75, 75, 55);
+//   indexer.set_value(true);
+//   pros::delay(300);
+//   indexer.set_value(false);
+//   pros::delay(300);
+//   indexer.set_value(true);
+//   pros::delay(300);
+//   indexer.set_value(false);
+//   pros::delay(300);
+// }
+
+// void auton2() {
+//   intake.move(127);
+//   flywheel.move(127);
+//   drive(90, 1, 0);
+//   pros::delay(300);
+//   drive(1, 10, -330);
+//   indexer.set_value(true);
+//   pros::delay(300);
+//   indexer.set_value(false);
+//   pros::delay(300);
+//   indexer.set_value(true);
+//   pros::delay(300);
+//   indexer.set_value(false);
+//   drive(1, 20, -325);
+//   indexer.set_value(true);
+//   pros::delay(300);
+//   indexer.set_value(false);
+//   pros::delay(300);
+//   indexer.set_value(true);
+//   pros::delay(300);
+//   indexer.set_value(false);
+//   drive(0, 20, -320);
+//   indexer.set_value(true);
+//   pros::delay(300);
+//   indexer.set_value(false);
+//   pros::delay(300);
+//   indexer.set_value(true);
+//   pros::delay(300);
+//   indexer.set_value(false);
+// }
+
+// void auton3() {
+//   flywheel.move(127);
+//   rotate(-10);
+//   indexer.set_value(true);
+//   pros::delay(300);
+//   indexer.set_value(false);
+// }
+
+void autonomous() {
+  	if(AUTON == 0){
+		angleOffset = 0;
+
+    turn(127, 270, 1);
+	}
+
+	else if(AUTON == 1){
+		angleOffset = 0;
+
+	}
+}
 
 /**
  * Runs the operator control code. This function will be started in its own task
