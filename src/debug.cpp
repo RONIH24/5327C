d9b7d93c313a5bdac0bb87192f82d76d14838ebf
Franklin Liu <fezhead@parabolas.xyz>
2022-10-20 20:07:28 -0700

    chart scaling

--- src/debug.cpp
+++ src/debug.cpp
@@ -13,24 +13,25 @@ void update(void *pain) {
   while (true) {
     double motor_rpm_1 = vroom.get_actual_velocity();
     double motor_rpm_2 = foom.get_actual_velocity();
-    printf("flywheel 1: %f, flywheel 2: %f\n", motor_rpm_1, motor_rpm_2);
+    // printf("flywheel 1: %f, flywheel 2: %f\n", motor_rpm_1, motor_rpm_2);
     lv_chart_set_next(wot->chart, wot->series1, motor_rpm_1);
     lv_chart_set_next(wot->chart, wot->series2, motor_rpm_2);
-    pros::delay(20);
+    pros::delay(100);
   }
 }
 
 // todo: add pros task
 Quack setup_graph(pros::Motor *motor1, pros::Motor *motor2) {
-  printf("Beginning setup_graph");
+  // printf("Beginning setup_graph");
   lv_obj_t *chart = lv_chart_create(lv_scr_act(), nullptr);
-  lv_obj_set_size(chart, 400, 200);
+  lv_obj_set_size(chart, 500, 200);
   lv_obj_align(chart, nullptr, LV_ALIGN_CENTER, 0, 0);
   lv_chart_set_type(chart, LV_CHART_TYPE_LINE);
 
   lv_chart_series_t *series1 = lv_chart_add_series(chart, LV_COLOR_BLUE);
   lv_chart_series_t *series2 = lv_chart_add_series(chart, LV_COLOR_RED);
 
+  lv_chart_set_range(chart, 0, 650);
   Quack wheat = Quack{chart, series1, series2, motor1, motor2};
   return wheat;
 }
